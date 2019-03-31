ea040d87e30ee98dbf82ab2b8f9bcd51ba212e18
Lucas Viana <lucasvddt2@gmail.com>
2019-03-31 20:50:03 -0300

    Criando layout da Main.

--- app/src/main/java/com/example/feiradasprofissoes/MainActivity.kt
+++ app/src/main/java/com/example/feiradasprofissoes/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.feiradasprofissoes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mTextView: TextView
+    private lateinit var mTextView2: TextView
+    private lateinit var mTextView3: TextView
+    private lateinit var mTextView4: TextView
+    private lateinit var mButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mTextView = findViewById(R.id.textView)
+        mTextView2 = findViewById(R.id.textView2)
+        mTextView3 = findViewById(R.id.textView3)
+        mTextView4 = findViewById(R.id.textView4)
+        mButton = findViewById(R.id.button)
     }
 }
